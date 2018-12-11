7eb32241bc8db5c194d718b6519f60235113110e
Frank Emrich <frank.emrich@ed.ac.uk>
2018-12-11 19:46:30 +0000

    Moved QualifiedName to its own file

--- core/sugartypes.ml
+++ core/sugartypes.ml
@@ -19,45 +19,6 @@ let with_pos           pos node   = { node; pos }
 let with_dummy_pos     node       = { node; pos = dummy_position }
 let tuple_of_with_pos {node; pos} = (node, pos)
 
-(* Identifiers *)
-module QualifiedName = struct
-  type t =
-    [ `Ident of string     (* identifier name x *)
-    | `Dot of string * t ] (* access to a module component A.B.C.x *)
-    [@@deriving show]
-
-  type path = string list
-
-  (* A.B.C.D == (A.(B.(C.D))) *)
-  let rec of_path = function
-    | []  -> assert false
-    | [x] -> `Ident x
-    | x :: xs -> `Dot (x, of_path xs)
-
-  let of_name x = `Ident x
-
-  let rec unqualify = function
-    | `Ident name -> name
-    | `Dot (_, path) -> unqualify path
-
-  let rec split = function
-    | `Ident x -> [x]
-    | `Dot (x, q) -> x :: split q
-
-  let canonical_name q =
-    String.concat "\\" (split q)
-
-  let is_qualified = function
-    | `Ident _ -> false
-    | `Dot _ -> true
-
-  let rec prefix n qname =
-    match n, qname with
-      | (0, _) -> failwith "Illegal prefix length"
-      | _, `Ident _ -> qname
-      | 1, `Dot (s, _) -> `Ident s
-      | _, `Dot (s, remainder) -> `Dot (s, prefix (n-1) remainder)
-end
 
 
 type binder = (name * Types.datatype option) with_pos
