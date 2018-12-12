29f1c6989941c16aa08bb5a2de0c2da09654feb2
Frank Emrich <frank.emrich@ed.ac.uk>
2018-12-12 03:21:48 +0000

    do not die on empty prelude

--- core/lib.ml
+++ core/lib.ml
@@ -1608,7 +1608,11 @@ let patch_prelude_funs tyenv =
   {tyenv with
      FrontendTypeEnv.var_env =
       List.fold_right
-        (fun (name, t) env -> Env.String.bind env (name, (prelude_id, t)))
+        (fun (name, t) env -> 
+          if Env.String.has env name then
+            Env.String.bind env (name, (prelude_id, t))
+          else
+            env)
         [("map", datatype "((a) -b-> c, [a]) -b-> [c]");
          ("concatMap", datatype "((a) -b-> [c], [a]) -b-> [c]");
          ("sortByBase", datatype "((a) -b-> (|_::Base), [a]) -b-> [a]");
